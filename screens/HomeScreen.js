4f022ad480e11dcfbab161bbf27152ec745b45ea
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-05-06 20:31:54 -0400

    Remove unnecessary placeholder text from HomeScreen for improved clarity

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,12 +8,7 @@ const HomeScreen = () => {
     <View>
       <Text>Home Screen</Text>
       <Text>Welcome to the Home Screen!</Text>
-      <Text>Explore our amazing furniture collection.</Text>
-      <Text>Check out our latest offers and discounts.</Text>
-      <Text>Contact us for any inquiries or support.</Text>
-      <Text>Follow us on social media for updates.</Text>
-      <Text>Thank you for visiting our app!</Text>
-      <Text>Enjoy your shopping experience!</Text>
+ 
     </View>
   );
 };
